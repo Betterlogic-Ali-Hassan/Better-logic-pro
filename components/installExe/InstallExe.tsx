35865d154f1f4c19e13b1aa8e69ecbf4060a556d
HassanThePassionate <maqsoodia777@gmail.com>
2025-01-13 15:53:31 +0500

    Update

--- components/installExe/InstallExe.tsx
+++ components/installExe/InstallExe.tsx
@@ -6,43 +6,41 @@ import HeroBtn from "../homePage/heroBtn/HeroBtn";
 const InstallExe = () => {
   return (
     <section className={styles.wrapper}>
-      <div className='my-container'>
-        <div className={styles.main}>
-          <div className={styles.wrap}>
-            <div className={styles.image}>
-              <div className={styles["img-wrap"]}>
-                <Image
-                  src='https://www.zen.com/wp-content/uploads/2022/08/qrcodesection_zen.svg'
-                  alt='img'
-                  height={140}
-                  width={140}
-                  className=''
-                />
-              </div>
-            </div>
-            <h2 className={styles.heading}>
-              Take Control with Better History - Install Now
-            </h2>
-            <div className={styles.description}>
-              <p>
-                Easily manage and organize your browsing history with Better
-                History. Install now and experience smarter history management!
-              </p>
-            </div>
-            <div className='module-button_repeater inner-wrapper'></div>
-            <div className='flex items-center justify-center gap-6 pb-[56px] flex-wrap'>
-              <HeroBtn />
-              <HeroBtn
-                className='bg-blue-500 !border-blue-500 '
-                title='Add to Edge'
-                icon='/edge.png'
-              />
-              <HeroBtn
-                title='Add to Firefox'
-                icon='/firefox.png'
-                className='bg-red-500 !border-red-500 '
+      <div className='max-lg:my-container'>
+        <div className={styles.wrap}>
+          <div className={styles.image}>
+            <div className={styles["img-wrap"]}>
+              <Image
+                src='https://www.zen.com/wp-content/uploads/2022/08/qrcodesection_zen.svg'
+                alt='img'
+                height={140}
+                width={140}
+                className=''
               />
             </div>
+          </div>
+          <h2 className={styles.heading}>
+            Take Control with Better History - Install Now
+          </h2>
+          <div className={styles.description}>
+            <p>
+              Easily manage and organize your browsing history with Better
+              History. Install now and experience smarter history management!
+            </p>
+          </div>
+          <div className='module-button_repeater inner-wrapper'></div>
+          <div className='flex items-center justify-center gap-6 pb-[56px] flex-wrap'>
+            <HeroBtn />
+            <HeroBtn
+              className='bg-blue-500 !border-blue-500 '
+              title='Add to Edge'
+              icon='/edge.png'
+            />
+            <HeroBtn
+              title='Add to Firefox'
+              icon='/firefox.png'
+              className='bg-red-500 !border-red-500 '
+            />
           </div>
         </div>
       </div>
